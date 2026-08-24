03f928dc9787c6f9a7e8761a50b5d7d239482729
niciolajboholt <nicolajbach12@gmail.com>
2026-08-24 17:26:14 +0200

    Add Assets/RuinAR/Editor/RuinARProjectBootstrapper.cs

---
+++ Assets/RuinAR/Editor/RuinARProjectBootstrapper.cs
@@ -0,0 +1,51 @@
+using System.IO;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+namespace RuinAR.Editor
+{
+    [InitializeOnLoad]
+    public static class RuinARProjectBootstrapper
+    {
+        private const string SceneDirectory = "Assets/RuinAR/Scenes";
+        private const string ScenePath = SceneDirectory + "/RuinARPrototype.unity";
+
+        static RuinARProjectBootstrapper()
+        {
+            EditorApplication.delayCall += EnsureProjectSetup;
+        }
+
+        [MenuItem("RuinAR/Configure prototype project")]
+        public static void EnsureProjectSetup()
+        {
+            if (EditorApplication.isPlayingOrWillChangePlaymode)
+                return;
+
+            PlayerSettings.companyName = "RuinAR";
+            PlayerSettings.productName = "RuinAR Prototype";
+            PlayerSettings.SetApplicationIdentifier(BuildTargetGroup.Android, "dk.ruinar.prototype");
+            PlayerSettings.SetApplicationIdentifier(BuildTargetGroup.iOS, "dk.ruinar.prototype");
+            PlayerSettings.defaultInterfaceOrientation = UIOrientation.AutoRotation;
+            PlayerSettings.Android.minSdkVersion = AndroidSdkVersions.AndroidApiLevel26;
+
+            if (!Directory.Exists(SceneDirectory))
+                Directory.CreateDirectory(SceneDirectory);
+
+            if (!File.Exists(ScenePath))
+            {
+                var scene = EditorSceneManager.NewScene(NewSceneSetup.EmptyScene, NewSceneMode.Single);
+                EditorSceneManager.SaveScene(scene, ScenePath);
+                Debug.Log("RuinAR: Prototype scene created.");
+            }
+
+            EditorBuildSettings.scenes = new[]
+            {
+                new EditorBuildSettingsScene(ScenePath, true)
+            };
+
+            AssetDatabase.SaveAssets();
+        }
+    }
+}
+
