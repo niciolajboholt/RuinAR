06af60bd8c46d40f96fdb5fce0330247f574c82a
niciolajboholt <nicolajbach12@gmail.com>
2026-08-24 17:28:44 +0200

    Add Assets/RuinAR/Scripts/Core/OfflineRuinPackageStore.cs

---
+++ Assets/RuinAR/Scripts/Core/OfflineRuinPackageStore.cs
@@ -0,0 +1,32 @@
+using System.IO;
+using UnityEngine;
+
+namespace RuinAR.Core
+{
+    public static class OfflineRuinPackageStore
+    {
+        private const string FileName = "prototype-ruin.json";
+
+        public static string PackagePath => Path.Combine(Application.persistentDataPath, FileName);
+
+        public static void Save(RuinSiteData site)
+        {
+            var json = JsonUtility.ToJson(site, true);
+            File.WriteAllText(PackagePath, json);
+        }
+
+        public static RuinSiteData LoadOrCreate()
+        {
+            if (!File.Exists(PackagePath))
+            {
+                var prototype = RuinSiteData.CreatePrototype();
+                Save(prototype);
+                return prototype;
+            }
+
+            var json = File.ReadAllText(PackagePath);
+            return JsonUtility.FromJson<RuinSiteData>(json) ?? RuinSiteData.CreatePrototype();
+        }
+    }
+}
+
